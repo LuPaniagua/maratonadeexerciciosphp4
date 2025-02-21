75071a38b8aedcc58c3155ded8ae1c001c0e9c8b
Luan Paniagua <luanvictor.castro11@gmail.com>
2025-02-21 08:41:38 -0300

    Update index.php

--- ex002/index.php
+++ ex002/index.php
@@ -18,6 +18,30 @@ for ($i = 0; $i < count($numeros); $i++) {
 $media = $soma / count($numeros);
 
 echo "A média dos números é: " . $media;
+
+$soma = 0;
+$numeros= [];
+
+//Laço para gerar 1 números inteiros aleatórios
+for ($i = 1; $i <= 10; $i++){
+    //Gera um número aleatório entre 1 e 100
+    $numero = rand(1,100);
+
+    //Adiciona o número à lista
+    $numeros[] = $numero;
+
+    //Adiciona o número à soma
+    $soma += $numero;
+}
+
+//Exibe os 10 números gerados
+echo "Os 10 números gerados são : " . implode(",", $numeros) . "<br>";
+
+//Calcula a média
+$media = $soma / 10;
+
+//Exibe a média
+echo "A média dos 10 números é: " . $media . "<br>";
 ?>
 
 </body>
